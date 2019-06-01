2195fad710d85a1bf9f1c9008d6908ccc8f6f3cb
Greg Tucker <greg.b.tucker@intel.com>
2019-05-31 17:49:22 -0700

    build: Add way to mark functions as deprecated or experimental
    Change-Id: I4edd15538c73ecfbbf4c588b93a01002b9c66a82
    Signed-off-by: Greg Tucker <greg.b.tucker@intel.com>

--- include/types.h
+++ include/types.h
@@ -64,6 +64,35 @@ extern "C" {
 # define DEBUG_PRINT(x) do {} while (0)
 #endif
 
+
+#ifndef __has_feature
+# define __has_feature(x) 0
+#endif
+#ifndef __has_extension
+# define __has_extension __has_feature
+#endif
+#define ISAL_GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+
+#if (defined(__ICC) || defined( __GNUC__ ) || defined(__clang__))
+# if __has_extension(attribute_deprecated_with_message) \
+	|| (ISAL_GCC_VERSION >= 40500) \
+	|| (__INTEL_COMPILER >= 1100)
+#   define ISAL_DEPRECATED(message) __attribute__(( deprecated( message )))
+# else
+#   define ISAL_DEPRECATED(message) __attribute__(( deprecated ))
+# endif
+#elif (defined( __ICL ) || defined(_MSC_VER))
+# if (__INTEL_COMPILER >= 1100) || (_MSC_FULL_VER >= 140050727)
+#   define ISAL_DEPRECATED(message) __declspec( deprecated ( message ))
+# else
+#   define ISAL_DEPRECATED(message) __declspec( deprecated )
+# endif
+#else
+# define ISAL_DEPRECATED(message)
+#endif
+
+#define ISAL_EXPERIMENTAL(message) ISAL_DEPRECATED("Experimental: " message)
+
 #ifdef __cplusplus
 }
 #endif
