6d4ee3d257e5177b2d362499777bc477119cae0d
Greg Tucker <greg.b.tucker@intel.com>
2021-04-16 18:38:34 -0700

    aes: Repair some extended test aligned_free
    Change-Id: I942b7a3d511ef75f50f8234ca2850496704b0a5b
    Signed-off-by: Greg Tucker <greg.b.tucker@intel.com>

--- aes/gcm_nt_rand_test.c
+++ aes/gcm_nt_rand_test.c
@@ -221,9 +221,11 @@ int check_vector(struct gcm_key_data *gkey, struct gcm_context_data *gctx, gcm_v
 	free(T_test);
 	free(o_T_test);
 	free(IV_c);
-	free(pt_test);
-	free(ct_test);
-	free(o_ct_test);
+	if (vector->Plen != 0) {
+		aligned_free(pt_test);
+		aligned_free(ct_test);
+		aligned_free(o_ct_test);
+	}
 
 	return OK;
 }
@@ -298,7 +300,7 @@ int check_strm_vector(struct gcm_key_data *gkey, struct gcm_context_data *gctx,
 		aes_gcm_enc_128_update_nt(gkey, gctx, vector->C + last_break, stream,
 					  i - last_break);
 		if (i - last_break != 0)
-			free(stream);
+			aligned_free(stream);
 
 		if (rand() % 1024 == 0) {
 			length = rand() % 100;
@@ -344,7 +346,7 @@ int check_strm_vector(struct gcm_key_data *gkey, struct gcm_context_data *gctx,
 			aes_gcm_dec_128_update_nt(gkey, gctx, vector->P + last_break, stream,
 						  i - last_break);
 			if (i - last_break != 0)
-				free(stream);
+				aligned_free(stream);
 
 			if (rand() % 1024 == 0) {
 				length = rand() % 100;
@@ -381,9 +383,11 @@ int check_strm_vector(struct gcm_key_data *gkey, struct gcm_context_data *gctx,
 	free(T_test);
 	free(o_T_test);
 	free(IV_c);
-	free(pt_test);
-	free(ct_test);
-	free(o_ct_test);
+	if (vector->Plen != 0) {
+		aligned_free(pt_test);
+		aligned_free(ct_test);
+		aligned_free(o_ct_test);
+	}
 	free(rand_data);
 
 	return OK;
@@ -457,7 +461,7 @@ int check_strm_vector2(struct gcm_key_data *gkey, struct gcm_context_data *gctx,
 		aes_gcm_enc_128_update_nt(gkey, gctx, vector->C + last_break, stream,
 					  i - last_break);
 		if (i - last_break != 0)
-			free(stream);
+			aligned_free(stream);
 		last_break = i;
 		i = i + (length - start) / breaks;
 
@@ -495,7 +499,7 @@ int check_strm_vector2(struct gcm_key_data *gkey, struct gcm_context_data *gctx,
 		aes_gcm_dec_128_update_nt(gkey, gctx, vector->P + last_break, stream,
 					  i - last_break);
 		if (i - last_break != 0)
-			free(stream);
+			aligned_free(stream);
 		last_break = i;
 		i = i + (length - start) / breaks;
 
@@ -519,6 +523,11 @@ int check_strm_vector2(struct gcm_key_data *gkey, struct gcm_context_data *gctx,
 		printf(" ISA-L->OpenSSL decryption failed Authentication\n");
 	OK |= (-1 == result);
 	free(rand_data);
+	if (vector->Plen != 0) {
+		free(pt_test);
+		free(ct_test);
+		aligned_free(o_ct_test);
+	}
 
 	return OK;
 }
@@ -591,7 +600,7 @@ int check_strm_vector_efence(struct gcm_key_data *gkey, struct gcm_context_data 
 			aes_gcm_enc_128_update_nt(gkey, gctx, vector->C + last_break,
 						  stream + PAGE_LEN - (i - last_break),
 						  i - last_break);
-			free(stream);
+			aligned_free(stream);
 
 			if (rand() % 1024 == 0) {
 				length = rand() % 100;
@@ -637,7 +646,7 @@ int check_strm_vector_efence(struct gcm_key_data *gkey, struct gcm_context_data 
 			aes_gcm_dec_128_update_nt(gkey, gctx, vector->P + last_break,
 						  stream + PAGE_LEN - (i - last_break),
 						  i - last_break);
-			free(stream);
+			aligned_free(stream);
 
 			if (rand() % 1024 == 0) {
 				length = rand() % 100;
@@ -674,9 +683,11 @@ int check_strm_vector_efence(struct gcm_key_data *gkey, struct gcm_context_data 
 	free(T_test);
 	free(o_T_test);
 	free(IV_c);
-	free(pt_test);
-	free(ct_test);
-	free(o_ct_test);
+	if (vector->Plen != 0) {
+		aligned_free(pt_test);
+		aligned_free(ct_test);
+		aligned_free(o_ct_test);
+	}
 	free(rand_data);
 
 	return OK;
@@ -776,9 +787,11 @@ int check_256_vector(struct gcm_key_data *gkey, struct gcm_context_data *gctx,
 	free(T_test);
 	free(o_T_test);
 	free(IV_c);
-	free(pt_test);
-	free(ct_test);
-	free(o_ct_test);
+	if (vector->Plen != 0) {
+		aligned_free(pt_test);
+		aligned_free(ct_test);
+		aligned_free(o_ct_test);
+	}
 
 	return OK;
 }
@@ -901,7 +914,7 @@ int check_256_strm_vector(struct gcm_key_data *gkey, struct gcm_context_data *gc
 		aes_gcm_dec_256_update_nt(gkey, gctx, vector->P + last_break, stream,
 					  i - last_break);
 		if (i - last_break != 0)
-			free(stream);
+			aligned_free(stream);
 
 		if (rand() % 1024 == 0) {
 			length = rand() % 100;
@@ -940,9 +953,11 @@ int check_256_strm_vector(struct gcm_key_data *gkey, struct gcm_context_data *gc
 	free(T_test);
 	free(o_T_test);
 	free(IV_c);
-	free(pt_test);
-	free(ct_test);
-	free(o_ct_test);
+	if (vector->Plen != 0) {
+		aligned_free(pt_test);
+		aligned_free(ct_test);
+		aligned_free(o_ct_test);
+	}
 
 	return OK;
 }
@@ -1023,13 +1038,13 @@ int test_gcm_strm_efence(void)
 		test.A -= offset;
 		free(test.A);
 		test.C -= offset;
-		free(test.C);
+		aligned_free(test.C);
 		test.IV -= offset;
 		free(test.IV);
 		test.K -= offset;
 		free(test.K);
 		test.P -= offset;
-		free(test.P);
+		aligned_free(test.P);
 		test.T -= offset;
 		free(test.T);
 	}
@@ -1119,13 +1134,13 @@ int test_gcm_strm_combinations(int test_len)
 		test.A -= offset;
 		free(test.A);
 		test.C -= offset;
-		free(test.C);
+		aligned_free(test.C);
 		test.IV -= offset;
 		free(test.IV);
 		test.K -= offset;
 		free(test.K);
 		test.P -= offset;
-		free(test.P);
+		aligned_free(test.P);
 		test.T -= offset;
 		free(test.T);
 	}
@@ -1212,13 +1227,13 @@ int test_gcm_combinations(void)
 		test.A -= offset;
 		free(test.A);
 		test.C -= offset;
-		free(test.C);
+		aligned_free(test.C);
 		test.IV -= offset;
 		free(test.IV);
 		test.K -= offset;
 		free(test.K);
 		test.P -= offset;
-		free(test.P);
+		aligned_free(test.P);
 		test.T -= offset;
 		free(test.T);
 	}
@@ -1305,13 +1320,13 @@ int test_gcm256_combinations(void)
 		test.A -= offset;
 		free(test.A);
 		test.C -= offset;
-		free(test.C);
+		aligned_free(test.C);
 		test.IV -= offset;
 		free(test.IV);
 		test.K -= offset;
 		free(test.K);
 		test.P -= offset;
-		free(test.P);
+		aligned_free(test.P);
 		test.T -= offset;
 		free(test.T);
 	}
@@ -1401,13 +1416,13 @@ int test_gcm256_strm_combinations(int test_len)
 		test.A -= offset;
 		free(test.A);
 		test.C -= offset;
-		free(test.C);
+		aligned_free(test.C);
 		test.IV -= offset;
 		free(test.IV);
 		test.K -= offset;
 		free(test.K);
 		test.P -= offset;
-		free(test.P);
+		aligned_free(test.P);
 		test.T -= offset;
 		free(test.T);
 	}
@@ -1627,9 +1642,9 @@ int test_gcm128_std_vectors(gcm_vector const *vector)
 	OK |= (-1 == result);
 	OK |= check_data(pt_test, vector->P, vector->Plen, "OSSL decrypted plain text (C)");
 	if (NULL != ct_test)
-		free(ct_test);
+		aligned_free(ct_test);
 	if (NULL != pt_test)
-		free(pt_test);
+		aligned_free(pt_test);
 	if (NULL != IV_c)
 		free(IV_c);
 	if (NULL != T_test)
@@ -1771,9 +1786,9 @@ int test_gcm256_std_vectors(gcm_vector const *vector)
 	OK |= (-1 == result);
 	OK |= check_data(pt_test, vector->P, vector->Plen, "OSSL decrypted plain text (C)");
 	if (NULL != ct_test)
-		free(ct_test);
+		aligned_free(ct_test);
 	if (NULL != pt_test)
-		free(pt_test);
+		aligned_free(pt_test);
 	if (NULL != IV_c)
 		free(IV_c);
 	if (NULL != T_test)
@@ -1895,13 +1910,13 @@ int test_gcm_strm_combinations2(int length, int start, int breaks)
 		test.A -= offset;
 		free(test.A);
 		test.C -= offset;
-		free(test.C);
+		aligned_free(test.C);
 		test.IV -= offset;
 		free(test.IV);
 		test.K -= offset;
 		free(test.K);
 		test.P -= offset;
-		free(test.P);
+		aligned_free(test.P);
 		test.T -= offset;
 		free(test.T);
 	}
