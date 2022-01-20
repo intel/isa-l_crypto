e30dbf4831db9dbaccc0c3a3a285de673e9664f3
H.J. Lu <hjl.tools@gmail.com>
2022-01-20 11:28:08 -0800

    Properly add .note.gnu.property section to assembly codes
    1. Partially revert
    
    commit 7f8ce0f8177a2d76dfa7fc57f3f9af08eaf259e2
    Author: John Kariuki <John.K.Kariuki@intel.com>
    Date:   Wed Jun 3 12:22:52 2020 -0700
    
        x86: add support for Intel CET
    
    which includes a hack to work around the old nasm which doesn't support
    
    section .note.gnu.property  note  alloc noexec align=8
    
    This hack doesn't work for downstream which doesn't use the hack.
    
    2. If Intel CET is enabled, require nasm with note section support to
    add
    
    section .note.gnu.property  note  alloc noexec align=N
    
    to assembly codes.
    
    Verified with
    
    $ CC="gcc -Wl,-z,cet-report=error -fcf-protection" CXX="g++ -Wl,-z,cet-report=error -fcf-protection" .../configure x86_64-linux
    $ make -j8
    
    on Tiger Lake.
    
    Signed-off-by: H.J. Lu <hjl.tools@gmail.com>

--- include/reg_sizes.asm
+++ include/reg_sizes.asm
@@ -346,6 +346,23 @@
 %define YWORD(reg) reg %+ y
 %define ZWORD(reg) reg %+ z
 
+%ifdef INTEL_CET_ENABLED
+ %ifdef __NASM_VER__
+  %if AS_FEATURE_LEVEL >= 10
+   %ifidn __OUTPUT_FORMAT__,elf32
+section .note.gnu.property  note  alloc noexec align=4
+DD 0x00000004,0x0000000c,0x00000005,0x00554e47
+DD 0xc0000002,0x00000004,0x00000003
+   %endif
+   %ifidn __OUTPUT_FORMAT__,elf64
+section .note.gnu.property  note  alloc noexec align=8
+DD 0x00000004,0x00000010,0x00000005,0x00554e47
+DD 0xc0000002,0x00000004,0x00000003,0x00000000
+   %endif
+  %endif
+ %endif
+%endif
+
 %ifidn __OUTPUT_FORMAT__,elf32
 section .note.GNU-stack noalloc noexec nowrite progbits
 section .text
