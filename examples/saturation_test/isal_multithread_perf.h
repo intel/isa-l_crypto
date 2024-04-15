c280984a7e166e23e921238a896ac085a4904a65
Marcel Cornu <marcel.d.cornu@intel.com>
2024-04-15 14:26:29 +0100

    examples: reformat with new coding style
    Signed-off-by: Marcel Cornu <marcel.d.cornu@intel.com>

--- examples/saturation_test/isal_multithread_perf.h
+++ examples/saturation_test/isal_multithread_perf.h
@@ -5,32 +5,45 @@
 #include "isa-l_crypto.h"
 
 /* multibuffer hash */
-void *md5_ossl_func(void *arg);
-void *md5_mb_func(void *arg);
-void *sha1_ossl_func(void *arg);
-void *sha1_mb_func(void *arg);
-void *sha256_ossl_func(void *arg);
-void *sha256_mb_func(void *arg);
-void *sha512_ossl_func(void *arg);
-void *sha512_mb_func(void *arg);
+void *
+md5_ossl_func(void *arg);
+void *
+md5_mb_func(void *arg);
+void *
+sha1_ossl_func(void *arg);
+void *
+sha1_mb_func(void *arg);
+void *
+sha256_ossl_func(void *arg);
+void *
+sha256_mb_func(void *arg);
+void *
+sha512_ossl_func(void *arg);
+void *
+sha512_mb_func(void *arg);
 
 /* aes */
-void *cbc_128_dec_func(void *arg);
-void *cbc_192_dec_func(void *arg);
-void *cbc_256_dec_func(void *arg);
-void *xts_128_enc_func(void *arg);
-void *xts_256_enc_func(void *arg);
-#define AAD_LENGTH   16
-void *gcm_128_enc_func(void *arg);
-void *gcm_256_enc_func(void *arg);
-
+void *
+cbc_128_dec_func(void *arg);
+void *
+cbc_192_dec_func(void *arg);
+void *
+cbc_256_dec_func(void *arg);
+void *
+xts_128_enc_func(void *arg);
+void *
+xts_256_enc_func(void *arg);
+#define AAD_LENGTH 16
+void *
+gcm_128_enc_func(void *arg);
+void *
+gcm_256_enc_func(void *arg);
 
 typedef struct {
-	char *name;
-	void *(*thread_func) (void *arg);
-	uint32_t rounds_nbuf;	/* bufs number of one processing round */
+        char *name;
+        void *(*thread_func)(void *arg);
+        uint32_t rounds_nbuf; /* bufs number of one processing round */
 } alg_method;
-
 
 /* Global parameters*/
 extern long long run_secs;
@@ -44,9 +57,10 @@ extern pthread_cond_t count_cond;
 extern volatile uint32_t count;
 
 extern int verbose;
-#define printfv(format, args...) { \
-	if (verbose) \
-		printf (format, ##args); \
-}
+#define printfv(format, args...)                                                                   \
+        {                                                                                          \
+                if (verbose)                                                                       \
+                        printf(format, ##args);                                                    \
+        }
 
 #endif /* ISAL_MULTITHREAD_PERF_H_ */
