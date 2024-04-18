9d5584062ace2f46784703af6e13a47e9a28313f
Pablo de Lara <pablo.de.lara.guarch@intel.com>
2024-04-18 23:09:31 +0100

    fips: add SHA256 self tests
    Signed-off-by: Pablo de Lara <pablo.de.lara.guarch@intel.com>

--- fips/sha_self_tests.c
+++ fips/sha_self_tests.c
@@ -37,16 +37,23 @@
 #include <string.h>
 
 #include "sha1_mb.h"
+#include "sha256_mb.h"
 
 #include "internal_fips.h"
 #include "types.h"
 #include "test.h"
 
 typedef uint32_t DigestSHA1[SHA1_DIGEST_NWORDS];
+typedef uint32_t DigestSHA256[SHA256_DIGEST_NWORDS];
 
 static const uint8_t msg[] = "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq";
 static const DigestSHA1 expResultDigest_sha1 = { 0x84983E44, 0x1C3BD26E, 0xBAAE4AA1, 0xF95129E5,
                                                  0xE54670F1 };
+
+static const DigestSHA256 expResultDigest_sha256 = {
+        0x248D6A61, 0xD20638B8, 0xE5C02693, 0x0C3E6039,
+        0xA33CE459, 0x64FF2167, 0xF6ECEDD4, 0x19DB06C1
+};
 
 static int
 _sha1_self_test(void)
@@ -77,12 +84,42 @@ _sha1_self_test(void)
         return 0;
 }
 
+static int
+_sha256_self_test(void)
+{
+
+        SHA256_HASH_CTX_MGR mgr;
+        SHA256_HASH_CTX ctxpool, *ctx = NULL;
+        uint32_t j;
+
+        sha256_ctx_mgr_init(&mgr);
+
+        // Init context before first use
+        hash_ctx_init(&ctxpool);
+
+        ctx = sha256_ctx_mgr_submit(&mgr, &ctxpool, msg, strlen((char *) msg), HASH_ENTIRE);
+
+        if (ctx == NULL)
+                ctx = sha256_ctx_mgr_flush(&mgr);
+
+        if (ctx) {
+                for (j = 0; j < SHA256_DIGEST_NWORDS; j++) {
+                        if (expResultDigest_sha256[j] != ctxpool.job.result_digest[j])
+                                return -1;
+                }
+        } else
+                return -1;
+
+        return 0;
+}
+
 int
 _sha_self_tests(void)
 {
         int ret;
 
         ret = _sha1_self_test();
+        ret |= _sha256_self_test();
 
         return ret;
 }
