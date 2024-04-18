03cbdc9d7fb08663b760ae7a1648ea068a4b6dfb
Marcel Cornu <marcel.d.cornu@intel.com>
2024-04-18 21:00:16 +0100

    aes: extend param check test with gcm api
    Signed-off-by: Marcel Cornu <marcel.d.cornu@intel.com>

--- aes/aes_param_test.c
+++ aes/aes_param_test.c
@@ -32,7 +32,9 @@
 #include "aes_keyexp.h"
 #include "aes_cbc.h"
 #include "aes_xts.h"
+#include "aes_gcm.h"
 #include "test.h"
+#include "aes/gcm_vectors.h"
 
 #ifdef SAFE_PARAM
 #define CHECK_RETURN(state, expected, func)                                                        \
@@ -49,12 +51,27 @@ typedef int (*aes_keyexp_func)(const uint8_t *, uint8_t *, uint8_t *);
 typedef int (*aes_cbc_func)(const void *, const uint8_t *, const uint8_t *, void *, const uint64_t);
 typedef int (*aes_xts_func)(const uint8_t *, const uint8_t *, const uint8_t *, const uint64_t,
                             const void *, void *);
+typedef int (*aes_gcm_func)(const struct gcm_key_data *, struct gcm_context_data *, uint8_t *,
+                            const uint8_t *, const uint64_t, const uint8_t *, const uint8_t *,
+                            const uint64_t, uint8_t *, const uint64_t);
+typedef int (*aes_gcm_init_func)(const struct gcm_key_data *, struct gcm_context_data *,
+                                 const uint8_t *, const uint8_t *, const uint64_t);
+typedef int (*aes_gcm_update_func)(const struct gcm_key_data *, struct gcm_context_data *,
+                                   uint8_t *, const uint8_t *, const uint64_t);
+typedef int (*aes_gcm_finalize_func)(const struct gcm_key_data *, struct gcm_context_data *,
+                                     uint8_t *, const uint64_t);
+typedef int (*aes_gcm_pre_func)(const int *, struct gcm_key_data *);
 
 struct test_func {
         union {
                 aes_keyexp_func keyexp_func_ptr;
                 aes_cbc_func cbc_func_ptr;
                 aes_xts_func xts_func_ptr;
+                aes_gcm_func gcm_func_ptr;
+                aes_gcm_init_func gcm_init_func_ptr;
+                aes_gcm_update_func gcm_update_func_ptr;
+                aes_gcm_finalize_func gcm_finalize_func_ptr;
+                aes_gcm_pre_func gcm_pre_func_ptr;
         };
         char *func_name;
 };
@@ -163,6 +180,209 @@ test_aes_xts_api(aes_xts_func aes_xts_func_ptr, const char *name, const int expa
         return 0;
 }
 
+static int
+test_aes_gcm_api(aes_gcm_func aes_gcm_func_ptr, const char *name)
+{
+        struct gcm_key_data gkey;
+        struct gcm_context_data gctx;
+        uint8_t buf[256] = { 0 };
+        uint8_t iv[GCM_IV_LEN] = { 0 };
+        uint8_t *aad = buf;
+        uint8_t *tag = buf;
+
+        // test null key data
+        CHECK_RETURN(
+                aes_gcm_func_ptr(NULL, &gctx, buf, buf, sizeof(buf), iv, aad, 16, tag, MAX_TAG_LEN),
+                ISAL_CRYPTO_ERR_NULL_EXP_KEY, name);
+
+        // test null context
+        CHECK_RETURN(
+                aes_gcm_func_ptr(&gkey, NULL, buf, buf, sizeof(buf), iv, aad, 16, tag, MAX_TAG_LEN),
+                ISAL_CRYPTO_ERR_NULL_CTX, name);
+
+        // test null dst
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, NULL, buf, sizeof(buf), iv, aad, 16, tag,
+                                      MAX_TAG_LEN),
+                     ISAL_CRYPTO_ERR_NULL_DST, name);
+
+        // test null dst with zero len
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, NULL, buf, 0, iv, aad, 16, tag, MAX_TAG_LEN),
+                     ISAL_CRYPTO_ERR_NONE, name);
+
+        // test null src
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, buf, NULL, sizeof(buf), iv, aad, 16, tag,
+                                      MAX_TAG_LEN),
+                     ISAL_CRYPTO_ERR_NULL_SRC, name);
+
+        // test null src with zero len
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, buf, NULL, 0, iv, aad, 16, tag, MAX_TAG_LEN),
+                     ISAL_CRYPTO_ERR_NONE, name);
+
+        // test invalid len
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, buf, buf, GCM_MAX_LEN + 1, iv, aad, 16, tag,
+                                      MAX_TAG_LEN),
+                     ISAL_CRYPTO_ERR_CIPH_LEN, name);
+
+        // test zero len
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, buf, buf, 0, iv, aad, 16, tag, MAX_TAG_LEN),
+                     ISAL_CRYPTO_ERR_NONE, name);
+
+        // test null iv
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, buf, buf, sizeof(buf), NULL, aad, 16, tag,
+                                      MAX_TAG_LEN),
+                     ISAL_CRYPTO_ERR_NULL_IV, name);
+
+        // test null aad
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, buf, buf, sizeof(buf), iv, NULL, 16, tag,
+                                      MAX_TAG_LEN),
+                     ISAL_CRYPTO_ERR_NULL_AAD, name);
+
+        // test null aad with zero len
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, buf, buf, sizeof(buf), iv, NULL, 0, tag,
+                                      MAX_TAG_LEN),
+                     ISAL_CRYPTO_ERR_NONE, name);
+
+        // test null tag
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, buf, buf, sizeof(buf), iv, aad, 16, NULL,
+                                      MAX_TAG_LEN),
+                     ISAL_CRYPTO_ERR_NULL_AUTH, name);
+
+        // test auth tag lens
+        for (int i = 5; i <= MAX_TAG_LEN + 1; i++)
+                if (i % 4 == 0)
+                        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, buf, buf, sizeof(buf), iv, aad,
+                                                      16, tag, i),
+                                     ISAL_CRYPTO_ERR_NONE, name);
+                else
+                        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, buf, buf, sizeof(buf), iv, aad,
+                                                      16, tag, i),
+                                     ISAL_CRYPTO_ERR_AUTH_TAG_LEN, name);
+        return 0;
+}
+
+static int
+test_aes_gcm_init_api(aes_gcm_init_func aes_gcm_func_ptr, const char *name)
+{
+        struct gcm_key_data gkey;
+        struct gcm_context_data gctx;
+        uint8_t iv[GCM_IV_LEN] = { 0 };
+        uint8_t aad[64] = { 0 };
+
+        // test null key data
+        CHECK_RETURN(aes_gcm_func_ptr(NULL, &gctx, iv, aad, sizeof(aad)),
+                     ISAL_CRYPTO_ERR_NULL_EXP_KEY, name);
+
+        // test null context
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, NULL, iv, aad, sizeof(aad)), ISAL_CRYPTO_ERR_NULL_CTX,
+                     name);
+
+        // test null iv
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, NULL, aad, sizeof(aad)),
+                     ISAL_CRYPTO_ERR_NULL_IV, name);
+
+        // test null aad
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, iv, NULL, sizeof(aad)),
+                     ISAL_CRYPTO_ERR_NULL_AAD, name);
+
+        // test null aad with zero len
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, iv, aad, 0), ISAL_CRYPTO_ERR_NONE, name);
+
+        return 0;
+}
+
+static int
+test_aes_gcm_update_api(aes_gcm_update_func aes_gcm_func_ptr, const char *name)
+{
+        struct gcm_key_data gkey;
+        struct gcm_context_data gctx;
+        uint8_t buf[256] = { 0 };
+
+        // test null key data
+        CHECK_RETURN(aes_gcm_func_ptr(NULL, &gctx, buf, buf, sizeof(buf)),
+                     ISAL_CRYPTO_ERR_NULL_EXP_KEY, name);
+
+        // test null context
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, NULL, buf, buf, sizeof(buf)), ISAL_CRYPTO_ERR_NULL_CTX,
+                     name);
+
+        // test null dst
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, NULL, buf, sizeof(buf)),
+                     ISAL_CRYPTO_ERR_NULL_DST, name);
+
+        // test null dst with zero len
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, NULL, buf, 0), ISAL_CRYPTO_ERR_NONE, name);
+
+        // test null src
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, buf, NULL, sizeof(buf)),
+                     ISAL_CRYPTO_ERR_NULL_SRC, name);
+
+        // test null src with zero len
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, buf, NULL, 0), ISAL_CRYPTO_ERR_NONE, name);
+
+        // test invalid len
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, buf, buf, GCM_MAX_LEN + 1),
+                     ISAL_CRYPTO_ERR_CIPH_LEN, name);
+
+        return 0;
+}
+
+static int
+test_aes_gcm_finalize_api(aes_gcm_finalize_func aes_gcm_func_ptr, const char *name,
+                          const gcm_key_size key_size)
+{
+        struct gcm_key_data gkey = { 0 };
+        struct gcm_context_data gctx = { 0 };
+        uint8_t tag[MAX_TAG_LEN] = { 0 };
+        uint8_t iv[GCM_IV_LEN] = { 0 };
+        uint8_t aad[64] = { 0 };
+
+        // init required for valid cases
+        if (key_size == BITS_128)
+                isal_aes_gcm_init_128(&gkey, &gctx, iv, aad, sizeof(aad));
+        else
+                isal_aes_gcm_init_256(&gkey, &gctx, iv, aad, sizeof(aad));
+
+        // test null key data
+        CHECK_RETURN(aes_gcm_func_ptr(NULL, &gctx, tag, MAX_TAG_LEN), ISAL_CRYPTO_ERR_NULL_EXP_KEY,
+                     name);
+
+        // test null context
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, NULL, tag, MAX_TAG_LEN), ISAL_CRYPTO_ERR_NULL_CTX,
+                     name);
+
+        // test null tag
+        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, NULL, MAX_TAG_LEN), ISAL_CRYPTO_ERR_NULL_AUTH,
+                     name);
+
+        // test auth tag lens
+        for (int i = 5; i <= MAX_TAG_LEN + 1; i++)
+                if (i % 4 == 0)
+                        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, tag, i), ISAL_CRYPTO_ERR_NONE,
+                                     name);
+                else
+                        CHECK_RETURN(aes_gcm_func_ptr(&gkey, &gctx, tag, i),
+                                     ISAL_CRYPTO_ERR_AUTH_TAG_LEN, name);
+        return 0;
+}
+
+static int
+test_aes_gcm_pre_api(aes_gcm_pre_func aes_gcm_func_ptr, const char *name)
+{
+        struct gcm_key_data gkey;
+        int key[8] = { 0 };
+
+        // test null key
+        CHECK_RETURN(aes_gcm_func_ptr(NULL, &gkey), ISAL_CRYPTO_ERR_NULL_KEY, name);
+
+        // test null key data
+        CHECK_RETURN(aes_gcm_func_ptr(key, NULL), ISAL_CRYPTO_ERR_NULL_EXP_KEY, name);
+
+        // test valid params
+        CHECK_RETURN(aes_gcm_func_ptr(key, &gkey), ISAL_CRYPTO_ERR_NONE, name);
+
+        return 0;
+}
+
 #endif /* SAFE_PARAM */
 
 int
@@ -224,6 +444,97 @@ main(void)
         for (int i = 0; i < DIM(xts_exp_test_funcs); i++) {
                 fail |= test_aes_xts_api(xts_exp_test_funcs[i].xts_func_ptr,
                                          xts_exp_test_funcs[i].func_name, 1);
+        }
+
+        /* Test AES-GCM enc / dec API */
+        const struct test_func gcm_test_funcs[] = {
+                { .gcm_func_ptr = isal_aes_gcm_enc_128, "isal_aes_gcm_enc_128" },
+                { .gcm_func_ptr = isal_aes_gcm_enc_256, "isal_aes_gcm_enc_256" },
+                { .gcm_func_ptr = isal_aes_gcm_dec_128, "isal_aes_gcm_dec_128" },
+                { .gcm_func_ptr = isal_aes_gcm_dec_256, "isal_aes_gcm_dec_256" },
+                { .gcm_func_ptr = isal_aes_gcm_enc_128_nt, "isal_aes_gcm_enc_128_nt" },
+                { .gcm_func_ptr = isal_aes_gcm_enc_256_nt, "isal_aes_gcm_enc_256_nt" },
+                { .gcm_func_ptr = isal_aes_gcm_dec_128_nt, "isal_aes_gcm_dec_128_nt" },
+                { .gcm_func_ptr = isal_aes_gcm_dec_256_nt, "isal_aes_gcm_dec_256_nt" },
+        };
+
+        for (int i = 0; i < DIM(gcm_test_funcs); i++) {
+                fail |= test_aes_gcm_api(gcm_test_funcs[i].gcm_func_ptr,
+                                         gcm_test_funcs[i].func_name);
+        }
+
+        /* Test AES-GCM init API */
+        const struct test_func gcm_init_test_funcs[] = {
+                { .gcm_init_func_ptr = isal_aes_gcm_init_128, "isal_aes_gcm_init_128" },
+                { .gcm_init_func_ptr = isal_aes_gcm_init_256, "isal_aes_gcm_init_256" },
+        };
+
+        for (int i = 0; i < DIM(gcm_init_test_funcs); i++) {
+                fail |= test_aes_gcm_init_api(gcm_init_test_funcs[i].gcm_init_func_ptr,
+                                              gcm_init_test_funcs[i].func_name);
+        }
+
+        /* Test AES-GCM update API */
+        const struct test_func gcm_update_test_funcs[] = {
+                { .gcm_update_func_ptr = isal_aes_gcm_enc_128_update,
+                  "isal_aes_gcm_enc_128_update" },
+                { .gcm_update_func_ptr = isal_aes_gcm_enc_256_update,
+                  "isal_aes_gcm_enc_256_update" },
+                { .gcm_update_func_ptr = isal_aes_gcm_dec_128_update,
+                  "isal_aes_gcm_dec_128_update" },
+                { .gcm_update_func_ptr = isal_aes_gcm_dec_256_update,
+                  "isal_aes_gcm_dec_256_update" },
+                { .gcm_update_func_ptr = isal_aes_gcm_enc_128_update_nt,
+                  "isal_aes_gcm_enc_128_update_nt" },
+                { .gcm_update_func_ptr = isal_aes_gcm_enc_256_update_nt,
+                  "isal_aes_gcm_enc_256_update_nt" },
+                { .gcm_update_func_ptr = isal_aes_gcm_dec_128_update_nt,
+                  "isal_aes_gcm_dec_128_update_nt" },
+                { .gcm_update_func_ptr = isal_aes_gcm_dec_256_update_nt,
+                  "isal_aes_gcm_dec_256_update_nt" },
+        };
+
+        for (int i = 0; i < DIM(gcm_update_test_funcs); i++) {
+                fail |= test_aes_gcm_update_api(gcm_update_test_funcs[i].gcm_update_func_ptr,
+                                                gcm_update_test_funcs[i].func_name);
+        }
+
+        /* Test AES-GCM finalize API */
+        const struct test_func gcm_finalize_128_test_funcs[] = {
+                { .gcm_finalize_func_ptr = isal_aes_gcm_enc_128_finalize,
+                  "isal_aes_gcm_enc_128_finalize" },
+                { .gcm_finalize_func_ptr = isal_aes_gcm_dec_128_finalize,
+                  "isal_aes_gcm_dec_128_finalize" },
+        };
+
+        for (int i = 0; i < DIM(gcm_finalize_128_test_funcs); i++) {
+                fail |= test_aes_gcm_finalize_api(
+                        gcm_finalize_128_test_funcs[i].gcm_finalize_func_ptr,
+                        gcm_finalize_128_test_funcs[i].func_name, BITS_128);
+        }
+
+        const struct test_func gcm_finalize_256_test_funcs[] = {
+                { .gcm_finalize_func_ptr = isal_aes_gcm_enc_256_finalize,
+                  "isal_aes_gcm_enc_256_finalize" },
+                { .gcm_finalize_func_ptr = isal_aes_gcm_dec_256_finalize,
+                  "isal_aes_gcm_dec_256_finalize" },
+        };
+
+        for (int i = 0; i < DIM(gcm_finalize_256_test_funcs); i++) {
+                fail |= test_aes_gcm_finalize_api(
+                        gcm_finalize_256_test_funcs[i].gcm_finalize_func_ptr,
+                        gcm_finalize_256_test_funcs[i].func_name, BITS_256);
+        }
+
+        /* Test AES-GCM pre API */
+        const struct test_func gcm_pre_test_funcs[] = {
+                { .gcm_pre_func_ptr = isal_aes_gcm_pre_128, "isal_aes_gcm_pre_128" },
+                { .gcm_pre_func_ptr = isal_aes_gcm_pre_256, "isal_aes_gcm_pre_256" },
+        };
+
+        for (int i = 0; i < DIM(gcm_pre_test_funcs); i++) {
+                fail |= test_aes_gcm_pre_api(gcm_pre_test_funcs[i].gcm_pre_func_ptr,
+                                             gcm_pre_test_funcs[i].func_name);
         }
 
         printf(fail ? "Fail\n" : "Pass\n");
